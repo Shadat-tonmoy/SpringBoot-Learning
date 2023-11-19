e34ef93619c87b0c72defbde7515a7ee5460d452
shadattonmoy <shadat.tonmoy@gmail.com>
2023-11-19 11:42:40 +0600

    - completed spring boot bean lifecycle method

--- SpringBootCore/src/main/java/com/example/springbootcore/demo/controllers/dataProvider/HockeyCoach.java
+++ SpringBootCore/src/main/java/com/example/springbootcore/demo/controllers/dataProvider/HockeyCoach.java
@@ -1,15 +1,30 @@
 package com.example.springbootcore.demo.controllers.dataProvider;
 
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
 import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component
-@Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
+//@Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
 public class HockeyCoach implements Coach {
 
     public HockeyCoach() {
         System.out.println("Constructor call of " + getClass().getSimpleName());
+    }
+
+
+    @PostConstruct
+    public void doInitialStuff() {
+        System.out.println("Do initial stuff called from : "+getClass().getSimpleName());
+    }
+
+
+    @PreDestroy
+    public void doCleanupStuff() {
+        System.out.println("Do cleanup stuff called from : "+getClass().getSimpleName());
+
     }
 
     @Override
